5522805ec606f535e3f253ad7d04f5effb2dc465
天川之约 <woaitianwen@qq.com>
2018-01-14 16:24:37 +0800

    修改中间件会话

--- middlewares/check.js
+++ middlewares/check.js
@@ -2,16 +2,16 @@ module.exports = {
 
     checkLogin(req, res, next) {
         if (!req.session.user) {
-            res.json({ error: true, message: "未登录！" })
+            return res.json({ error: true, message: "未登录！" })
         }
-        next();
+        next()
     },
   
     checkNotLogin(req, res, next) {
         if (req.session.user) {
-            es.json({ error: false, message: "已经登录！" })
+            return res.json({ error: false, message: "已经登录！" })
         }
-        next();
+        next()
     }
 
 }
