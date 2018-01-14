ef730fab78345a5664b77044ad63dd214e19857f
天川之约 <woaitianwen@qq.com>
2018-01-14 21:45:47 +0800

    增加图书的会话功能

--- router/book.js
+++ router/book.js
@@ -1,21 +1,25 @@
 const express = require('express')
 const router = express.Router()
 const Book = require('../models/book')
+const checkLogin = require('../middlewares/check').checkLogin
+//const checkNotLogin = require('../middlewares/check').checkNotLogin
 
 //查询所有书籍
-router.get('/book', (req, res) => {
-    Book.find({})
-        .sort({ update_at : -1})
-        .then(books => {
-            res.json(books)
-        })
-        .catch(err => {
-            res.json(err)
-        })
+router.get('/book', checkLogin ,(req, res) => {
+
+        Book.find({})
+            .sort({ update_at: -1 })
+            .then(books => {
+                res.json(books)
+            })
+            .catch(err => {
+                res.json(err)
+            })
+    
 })
 
 //查询一个书籍
-router.get('/book/:id', (req, res) => {
+router.get('/book/:id', checkLogin , (req, res) => {
     Book.findById(req.params.id, (err, book) => {
         if (err) {
             res.json(err)
@@ -26,7 +30,7 @@ router.get('/book/:id', (req, res) => {
 })
 
 //模糊查询一个书籍名称
-router.get('/', (req, res) => {
+router.get('/', checkLogin ,(req, res) => {
     if (req.query.search) {
         const regex = new RegExp(escapeRegex(req.query.search), 'gi');
         Book.find({ title: regex }, (err, allbook) => {
@@ -46,7 +50,7 @@ router.get('/', (req, res) => {
 })
 
 //添加一个书籍
-router.post('/book', (req, res) => {
+router.post('/book', checkLogin ,(req, res) => {
     if (!req.body.title || !req.body.author || !req.body.price || !req.body.introduction || !req.body.img_url) {
         return res.status(403).json({ "error": "true", "message": "缺少一个参数！请填写完整的内容！"})
     } else {
@@ -63,7 +67,7 @@ router.post('/book', (req, res) => {
 })
 
 //更新一个书籍
-router.put('/book/:id', (req, res) => {
+router.put('/book/:id', checkLogin , (req, res) => {
     Book.findByIdAndUpdate({ _id: req.params.id}, { $set: { 
         title: req.body.title,
         author: req.body.author,
@@ -79,7 +83,7 @@ router.put('/book/:id', (req, res) => {
 })
 
 //删除一个书籍
-router.delete('/book/:id', (req, res) => {
+router.delete('/book/:id', checkLogin , (req, res) => {
     Book.findOneAndRemove({
         _id: req.params.id
     })
