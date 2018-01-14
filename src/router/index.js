f25e333c91e0a547e64093dea5ba4f5086fdc0d9
天川之约 <woaitianwen@qq.com>
2018-01-14 16:49:00 +0800

    增加登录路由

--- src/router/index.js
+++ src/router/index.js
@@ -2,11 +2,17 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import BookList from '@/components/BookList'
 import BookDetail from '@/components/BookDetail'
+import Login from '@/components/user/login'
 
 Vue.use(Router)
 
 export default new Router({
   routes: [
+    {
+      path: '/login',
+      name: '登录',
+      component: Login
+    },
     {
       path: '/',
       name: 'BookList',
