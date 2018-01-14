8e76c685e92780cce540454633f587026471d9a1
天川之约 <woaitianwen@qq.com>
2018-01-15 03:30:26 +0800

    修改默认接口

--- router/index.js
+++ router/index.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 
-router.get('/', (req, res) => {
-    res.send('Hello Express!')
+router.get('/api', (req, res) => {
+    //res.send('kkkk')
+    res.json({message: '欢迎来到API羽翼图书接口！'})
 })
 
 module.exports = router
