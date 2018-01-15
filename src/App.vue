151fc967cb8b4cb2d683bf2b7f035c2b5bc1b653
天川之约 <woaitianwen@qq.com>
2018-01-15 23:54:19 +0800

    暂时去掉登录按钮

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,13 @@
 <template>
   <div id="app">
-    <!--<mu-appbar :title="title" class="title" :zDepth="zDepth"/>-->
+<mu-appbar :title="title">
+    <mu-icon-menu icon="more_vert" slot="right">
+      <mu-menu-item @click="homeClick" title="首页"/>
+      <mu-menu-item @click="backClcik" title="后台"/>
+
+    </mu-icon-menu>
+</mu-appbar>
+
     <router-view></router-view>
   </div>
 </template>
@@ -8,10 +15,28 @@
 <script>
 
 export default {
+  /*created(){
+    
+  },*/
   data(){
     return {
+      title: '羽翼图书',
     }
+  }, 
+
+  methods: {
+    homeClick() {
+      this.$router.push('/')
+      //alert('首页')
+    },
+    backClcik() {
+      this.$router.push('/bookList')
+       //alert('后台')
+    },
+
   }
+  
+
 }
 </script>
 <style>
@@ -25,9 +50,10 @@ export default {
   justify-content:center;
   align-items:flex-start;
 }*/
-#app {
+/*#app {
   text-align: center;
-}
+}*/
+
 </style>
 
 
